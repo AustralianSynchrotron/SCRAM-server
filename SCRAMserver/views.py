eb101eb6d29c1dda7a608fea89e91557d2c32b77
Cameron Rodda <cameron.rodda@synchrotron.org.au>
2017-03-21 10:22:07 +1100

    changed post request

--- SCRAMserver/views.py
+++ SCRAMserver/views.py
@@ -1,4 +1,5 @@
 from SCRAMserver import app,logger
+from flask import request
 
 
 @app.route('/')
@@ -6,7 +7,8 @@ def hello_world():
     return 'Hello World! Just imagine a fancy new web interface to the <b>Superior Control Room Alarm Management</b> software right here on this page'
 
 
-@app.route('/input/<source>/<input_string>', methods=['POST'])
-def input(source,input_string):
+@app.route('/input/<source>/', methods=['POST'])
+def input(source):
     if source == 'ALH':
+        input_string = request.form['alh_string']
         logger.info("Received [%s]: %s" % (source,input_string))
