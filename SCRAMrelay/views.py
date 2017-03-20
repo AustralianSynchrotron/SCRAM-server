41eb5cd814080479e193dbdbcec0431ca35d36aa
Cameron Rodda <cameron.rodda@synchrotron.org.au>
2017-03-21 10:24:09 +1100

    created a relay app to forward requests across the network

---
+++ SCRAMrelay/views.py
@@ -0,0 +1,19 @@
+from SCRAMrelay import app,logger
+from flask import request
+import requests
+
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World! Just imagine a fancy new web RELAY interface to the <b>Superior Control Room Alarm Management</b> software right here on this page'
+
+
+@app.route('/relay/<source>/', methods=['POST'])
+def input(source):
+    if source == 'ALH':
+        input_string = request.form['alh_string']
+        logger.info("Received [%s]: %s" % (source,input_string))
+        url = "10.6.0.198:9669/input/%s/" % (source)
+        r = requests.post( url , data={"alh_string":input_string})
+        logger.info("Forwarded [%s]: %s to %s with result: %s" % (source,input_string,url,r.text))
---
+++ run_SCRAM-relay.py
@@ -0,0 +1,6 @@
+from SCRAMrelay import app
+
+app.debug = True
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9668)
