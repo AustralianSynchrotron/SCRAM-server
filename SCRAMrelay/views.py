9fde2cdcb850978d8ee55019e1507343f122ba74
Cameron Rodda <cameron.rodda@synchrotron.org.au>
2017-04-03 14:25:24 +1000

    changed ip for node server

--- SCRAMrelay/views.py
+++ SCRAMrelay/views.py
@@ -13,7 +13,7 @@ def input(source):
     if source == 'ALH':
         input_string = request.form['alh_string']
         logger.info("Received [%s]: %s" % (source,input_string))
-        url = "http://10.6.100.198:9669/input/%s/" % (source)
+        url = "http://10.6.100.198:9667/input/%s/" % (source)
         r = requests.post( url , data={"alh_string":input_string})
         logger.info("Forwarded [%s]: %s to %s with result: %s" % (source,input_string,url,r.text))
 
